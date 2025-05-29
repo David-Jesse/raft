99ec59172ec29b0603373cedb1c1077d78b4ec47
David-Jesse <djesse351@gmail.com>
2025-05-29 12:10:40 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,100 @@
 import Image from "next/image";
+import Link from "next/link";
+import leftArrow from "../assets/images/leftArrow.png";
+import chinaBlack from "../assets/images/chinaBlack.png";
+import forbesLogo from "../assets/images/forbesLogo.png";
+import cnbc from "../assets/images/cnbc.png";
+import bloomberg from "../assets/images/bloomberg.png";
+import cnn from "../assets/images/cnn.png";
+import reuters from "../assets/images/reuters.png";
+import smart from "../assets/images/smart.png";
+import seamless from "../assets/images/seamless.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-[#131313] min-h-screen w-full pt-20">
+      <div className="flex flex-col mx-auto w-[90%]">
+        {/* Hero Section */}
+        <div className="flex flex-col items-center justify-center gap-6">
+          <Link href="/">
+            <button className="py-1 px-3 rounded-full text-base bg-[#989898] hover:bg-[#989898]/80 flex items-center gap-2 cursor-pointer">
+              Introducing Raft Cards{" "}
+              <Image src={leftArrow} alt="Left Arrow" width={7} height={7} />
+            </button>
+          </Link>
+          <h1 className="text-white text-8xl w-[60%] text-center">
+            Building the future of banking.
+          </h1>
+          <p className="text-[#bdbdbd] text-2xl w-[50%] text-center">
+            Experience the future of banking with RAFT. We're here to empower
+            your financial journey.
+          </p>
+          <button className="bg-[#2B892E] py-2 px-6 text-white text-base rounded-full hover:bg-[#2B892E]/80 cursor-pointer">
+            Get Started
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </div>
+
+      {/* Top main Section */}
+      <div className="w-[90%] py-20 mx-auto">
+        <div>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={chinaBlack}
+            alt="Friends laughing"
+            width={2000}
+            height={1000}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        <div className="flex flex-col mx-auto justify-center items-center py-20">
+          <h3 className="text-[#BDBDBD] text-xl mb-10">FEATURED AND SEEN IN</h3>
+          <div className="flex gap-10">
+            <Image
+              src={forbesLogo}
+              alt="Forbes Logo"
+              className="object-contain"
+            />
+            <Image src={cnbc} alt="NBC Logo" className="object-contain" />
+            <Image
+              src={bloomberg}
+              alt="Bloomberg Logo"
+              className="object-contain"
+            />
+            <Image
+              src={reuters}
+              alt="Reuters Logo"
+              className="object-contain"
+            />
+            <Image src={cnn} alt="CNN Logo" className="object-contain" />
+          </div>
+        </div>
+
+        <div className="flex flex-col mx-auto justify-center items-center py-20">
+          <h2 className="text-white text text-7xl w-[60%] text-center mb-6">
+            Elevate Your Financial Journey with RAFT
+          </h2>
+          <p className="text-[#BDBDBD] text-xl w-[50%] text-center">
+            RAFT offers a world of financial possibilities. From investments to
+            payments we've got you covered. Join us and unlock your financial
+            potential today.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex gap-6 py-12 mx-auto justify-center">
+        <div className="w-[65%] border-2 p-5 rounded">
+          <div>
+            <Image
+              src={seamless}
+              alt="Seamless Payments"
+              className="object-contain"
+            />
+          </div>
+         
+         <h4 className="text-white text-3xl">Seamless Payment</h4>
+         <p className="text-white text-base">Enjoy secure, seamless transactions that make managing your money a breeze</p> 
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import navLogo from "../assets/images/navLogo.png";
+
+
+export default function Navbar() {
+  return (
+    <div className="w-full bg-[#131313]">
+      <div className=" w-[90%] mx-auto py-6 flex items-center justify-between">
+        {/* Logo here */}
+        <Link href='/' className="text-white flex items-center text-2xl gap-1">
+          <Image
+            src={navLogo}
+            alt="Raft logo"
+            width={30}
+            height={40}
+          />
+          Raft
+        </Link>
+
+        <div className="flex gap-4 text-[#BDBDBD] text-lg  transiton-all duration-200">
+          <Link href="#" className='hover:text-[#2B892E]'>Solutions</Link>
+          <Link href="#" className='hover:text-[#2B892E]'>Learn</Link>
+          <Link href="#" className='hover:text-[#2B892E]'>About</Link>
+        </div>
+
+        <div className="flex gap-2 text-white items-center">
+          <Link href="#" className="text-lg hover:text-[#2B892E]">Login</Link>
+          <button className="bg-[#2B892E] py-1 px-4 text-base rounded-full hover:bg-[#2B892E]/80 cursor-pointer" aria-label="Get Started with Raft">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
